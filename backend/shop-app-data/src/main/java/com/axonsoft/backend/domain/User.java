db03ed2a5c8f611f5a8ad095777196a514813c99
Alexandra <alelupas21@gmail.com>
2021-08-25 13:49:38 +0300

    added updateProduct, findByUsername + tests and multi-module

---
+++ backend/shop-app-data/src/main/java/com/axonsoft/backend/domain/User.java
@@ -0,0 +1,42 @@
+package com.axonsoft.backend.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    private String firstname;
+    private String lastname;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonManagedReference
+    private Set<Order> orders = new HashSet<>();
+
+    @Builder
+    public User(Long id, String firstname, String lastname, String username, String password, Set<Order> orders) {
+        super(id);
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.password = password;
+        if (orders != null)
+            this.orders = orders;
+    }
+
+}
---
+++ backend/shopapp-web/src/main/java/com/axonsoft/backend/ShopappWebApplication.java
@@ -0,0 +1,13 @@
+package com.axonsoft.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopappWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopappWebApplication.class, args);
+    }
+
+}
